c6a207a2457cb8145cb689b0f417df36a87ce354
Suian98 <ianbbpp@gmail.com>
2020-08-16 17:47:06 +0200

    arreglos menores

--- data/random.js
+++ data/random.js
@@ -9,7 +9,7 @@ function random() {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/lpCoy_syMsg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 2) {
-        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/T2wH8VAV6kY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/vDjTR0fzcdA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 3) {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/6ahdGIX2aVo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
@@ -24,7 +24,7 @@ function random() {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/EhgDibw7vB4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 7) {
-        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/vJ3jNTH2QL0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/lNTUFgwBzAw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 8) {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/omT5pc_R72g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
@@ -40,13 +40,13 @@ function random() {
         alert(txt);  
         }
     if (number == 10) {
-        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/8DuxysWsGBQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/3GYtJg_iKO4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 11) {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/7bqEl2RSE0E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 12) {
-        document.body.style.backgroundImage = "url('https://media.tenor.com/images/ef2866539433c68144425eaf4b983594/tenor.gif')";
+        document.body.style.backgroundImage = "url('https://media1.tenor.com/images/5d1042e930fb383184fba35db98f2f04/tenor.gif?itemid=14340305')";
 
         var audio = new Audio('https://s7.converto.io/download-file/pWTxMvgBs5DDZSLxOMIm1hD8oR8wrItd/file.mp3');
         audio.play();
@@ -60,10 +60,10 @@ function random() {
         document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="http://fakeupdate.net/win7/" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 15) {
-        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/kPQ-vrdhzYk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/iprV6Y9Qj8I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 16) {
-        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/kENellW9TF8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById('video').innerHTML = '<iframe width="720" height="620" src="https://www.youtube.com/embed/bkPDLq0lDJo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     if (number == 17) {
         alert("Samderx guapo");    
